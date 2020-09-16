99e9761fa5a725b54ecee4140eb3646fa13405e8
Callum Gray <cgray95@gmail.com>
2020-09-16 21:43:52 +1000

    Update Guessing-Game-MDP.py

--- Examples/Guessing-Game-MDP.py
+++ Examples/Guessing-Game-MDP.py
@@ -40,28 +40,29 @@ R = np.array([[R_func(s, a) for a in A] for s in S])
 ε = 10**(-3)
 
 
-print("\nVALUE ITERATION")    
-print("-" * 50)
-for γ in [0.25, 0.6, 0.95, 0.99]:
-    start = time.time()
-    V, π = VI(S, A, P, R, ε, γ)
-    V = [round(v, 6) for v in V]
-    π = [int(a + 1) for a in π]
-    stop = time.time()
-    print("Discount Factor =", γ)
-    print("  Time:", stop - start)  
-    print(" Value:", V, "\nPolicy:", π)
-    print('-' * 50)  
-
-print("\nPOLICY ITERATION")
-print("-" * 50)
-for γ in [0.25, 0.6, 0.95, 0.99]:
-    start = time.time()
-    V, π = PI(S, A, P, R, ε, γ)
-    V = [round(v, 6) for v in V]
-    π = [int(a + 1) for a in π]
-    stop = time.time()
-    print("Discount Factor =", γ)
-    print("  Time:", stop - start)  
-    print(" Value:", V, "\nPolicy:", π)
-    print('-' * 50)
+if __name__ == "__main__":
+    print("\nVALUE ITERATION")    
+    print("-" * 50)
+    for γ in [0.25, 0.6, 0.95, 0.99]:
+        start = time.time()
+        V, π = VI(S, A, P, R, ε, γ)
+        V = [round(v, 6) for v in V]
+        π = [int(a + 1) for a in π]
+        stop = time.time()
+        print("Discount Factor =", γ)
+        print("  Time:", stop - start)  
+        print(" Value:", V, "\nPolicy:", π)
+        print('-' * 50)  
+    
+    print("\nPOLICY ITERATION")
+    print("-" * 50)
+    for γ in [0.25, 0.6, 0.95, 0.99]:
+        start = time.time()
+        V, π = PI(S, A, P, R, ε, γ)
+        V = [round(v, 6) for v in V]
+        π = [int(a + 1) for a in π]
+        stop = time.time()
+        print("Discount Factor =", γ)
+        print("  Time:", stop - start)  
+        print(" Value:", V, "\nPolicy:", π)
+        print('-' * 50)
